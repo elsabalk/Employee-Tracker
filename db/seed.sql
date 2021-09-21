286c7a75ad10d0a14f1e78a5074291851006c1be
Elsa Balkaran <elsabalk@hotmail.com>
2021-09-20 21:51:55 -0500

    Updated seeds.sql and README.md

--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,15 @@
-'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employees AS emp, employee_db.role as role, 
+'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employee AS emp, employee_db.role as role, 
 employee_db.department as department WHERE emp.role_id = role.id and role.department_id = department.id';
 
-'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employees AS emp, employee_db.role as role, 
+'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employee AS emp, employee_db.role as role, 
 employee_db.department as department WHERE emp.role_id = role.id and role.department_id = department.id order by department_id';
 
-'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employees AS emp, employee_db.role as role, employee_db.department as department WHERE emp.role_id = role.id and role.department_id = department.id order by manager_id';
+'SELECT emp.id, emp.first_name, emp.last_name, role.title, role.salary, department.name as department, emp.manager_id as manager FROM employee_db.employee AS emp, employee_db.role as role, employee_db.department as department WHERE emp.role_id = role.id and role.department_id = department.id order by manager_id';
 
-'INSERT INTO employee_db.employees values ('${answers.id}', '${answers.firstName}', '${answers.lastName}', ${answers.roleId}, ${answers.managerId})';
+'INSERT INTO employee_db.employee values ('${answers.id}', '${answers.firstName}', '${answers.lastName}', ${answers.roleId}, ${answers.managerId})';
 
-'DELETE from employee_db.employees where id = '${answers.id}'`';
+'DELETE from employee_db.employee where id = '${answers.id}'`';
 
-'UPDATE employee_db.employees set role_id ='${answers.roleId}' where id= '${answers.employeeId}'`';
+'UPDATE employee_db.employee set role_id ='${answers.roleId}' where id= '${answers.employeeId}'`';
 
 'INSERT INTO employee_db.department values('${answers.departmentId}','${answers.departmentName}', 0);`';
